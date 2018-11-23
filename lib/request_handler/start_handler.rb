9ef8a1b76558f7a3dd72a5fcb8dfb98ebd2aaaff
Dmitry Polkhov <drholera@gmail.com>
2018-11-23 10:19:38 +0200

    Added check for start command

--- lib/request_handler/start_handler.rb
+++ lib/request_handler/start_handler.rb
@@ -8,6 +8,9 @@ class StartHandler < RequestHandlerBase
       user = User.find_by chat_id: @request.chat.id
       if user.nil?
         User.create(chat_id: @request.chat.id, enabled: true).save
+      elsif user.enabled?
+        @bot.api.send_message(chat_id: @request.chat.id, text: "You have already enabled the bot. Please use /help command to see the list of available commands")
+        return
       end
       user.enabled = true
       user.save
