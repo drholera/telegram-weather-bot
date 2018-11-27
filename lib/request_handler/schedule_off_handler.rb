a1e7a1112e95e9433ad3729cabb709fc69a97d43
Dmitry Polkhov <drholera@gmail.com>
2018-11-27 12:06:09 +0200

    Added schedule off handler

---
+++ lib/request_handler/schedule_off_handler.rb
@@ -0,0 +1,20 @@
+require_relative 'request_handler_base'
+
+class ScheduleOffHandler < RequestHandlerBase
+
+  def handle
+    super
+    if @user.enabled?
+      @user.schedule_enabled = false
+      @user.save
+      if !@user.schedule_enabled
+        @bot.api.send_message(chat_id: @request.chat.id, text: "Schedule has been disabled.")
+      else
+        @bot.api.send_message(chat_id: @request.chat.id, text: "Something went wrong. Schedule cannot be disabled at the moment. Please, try again later.")
+      end
+    else
+      @bot.api.send_message(chat_id: @request.chat.id, text: "You must enable bot before using commands. Please, run /start command.")
+    end
+  end
+
+end
