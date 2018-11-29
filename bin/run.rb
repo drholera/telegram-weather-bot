6df9bc71ad730987b89f2b5c3d52c68b29b48f62
Dmitry Polkhov <drholera@gmail.com>
2018-11-29 13:56:38 +0200

    Moved main loop to the separate module

--- bin/run.rb
+++ bin/run.rb
@@ -5,39 +5,8 @@ require_relative '../lib/helpers/last_command'
 require_relative '../config/config'
 
 include Config
+include Loader
 
 loop do
-  begin
-    Telegram::Bot::Client.run(Config::BOT_TOKEN) do |bot|
-      bot.listen do |rqst|
-        # @Todo. Move inner functionality to the separate class.
-        Thread.start(rqst) do |rqst|
-          if rqst.text and rqst.text[0] == "/"
-            command = rqst.text[1..-1]
-            LastCommand::set_last_command(rqst.chat.id, command)
-          else
-            command = LastCommand::get_last_command(rqst.chat.id)
-          end
-
-          handler_class_name = command[0].capitalize + command[1..-1] + "Handler"
-          if HandlerFactory.class_exists?(handler_class_name)
-            handler = HandlerFactory.get_instance(handler_class_name, bot, rqst)
-            handler.handle
-          else
-            @bot.api.send_message(chat_id: @request.chat.id, text: "Sorry, cannot find such a command")
-          end
-        end
-      end
-    end
-  rescue Exception => e
-    file_path = '../tmp/error.txt'
-    dirname = File.dirname(file_path)
-    unless File.directory?(dirname)
-      Dir.mkdir(dirname)
-    end
-    logger = Logger.new(file_path)
-    logger.level = Logger::FATAL
-    logger.fatal e.message
-    exit 0
-  end
+  main_loop
 end
--- lib/load.rb
+++ lib/load.rb
@@ -17,3 +17,43 @@ require_relative '../lib/helpers/weather_extractor'
 
 connection_details = YAML.load_file('../config/database.yml')
 ActiveRecord::Base.establish_connection(connection_details)
+
+module Loader
+
+  def main_loop
+    begin
+      Telegram::Bot::Client.run(Config::BOT_TOKEN) do |bot|
+        bot.listen do |rqst|
+          # @Todo. Move inner functionality to the separate class.
+          Thread.start(rqst) do |rqst|
+            if rqst.text and rqst.text[0] == "/"
+              command = rqst.text[1..-1]
+              LastCommand::set_last_command(rqst.chat.id, command)
+            else
+              command = LastCommand::get_last_command(rqst.chat.id)
+            end
+
+            handler_class_name = command[0].capitalize + command[1..-1] + "Handler"
+            if HandlerFactory.class_exists?(handler_class_name)
+              handler = HandlerFactory.get_instance(handler_class_name, bot, rqst)
+              handler.handle
+            else
+              @bot.api.send_message(chat_id: @request.chat.id, text: "Sorry, cannot find such a command")
+            end
+          end
+        end
+      end
+    rescue Exception => e
+      file_path = '../tmp/error.txt'
+      dirname = File.dirname(file_path)
+      unless File.directory?(dirname)
+        Dir.mkdir(dirname)
+      end
+      logger = Logger.new(file_path)
+      logger.level = Logger::FATAL
+      logger.fatal e.message
+      exit 0
+    end
+  end
+
+end
